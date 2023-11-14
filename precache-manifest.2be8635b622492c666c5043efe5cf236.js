8fe4d1618923af147da11b1dd1192bbd5504e659
Mayurdeep Baruah <115894333+MayurdeepBaruah@users.noreply.github.com>
2023-11-14 20:21:29 +0530

    Updates

---
+++ precache-manifest.2be8635b622492c666c5043efe5cf236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242bd01bfb388032d25e35cdf6cdf5c7",
+    "url": "/Textutils/index.html"
+  },
+  {
+    "revision": "6cdb3af4b84b64c818af",
+    "url": "/Textutils/static/css/main.04ffed66.chunk.css"
+  },
+  {
+    "revision": "bf5c34c705ad50e3c406",
+    "url": "/Textutils/static/js/2.af3fc02c.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/Textutils/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "6cdb3af4b84b64c818af",
+    "url": "/Textutils/static/js/main.aead4bad.chunk.js"
+  },
+  {
+    "revision": "b5305c03e5a04519dd92",
+    "url": "/Textutils/static/js/runtime~main.a6dbe6f9.js"
+  }
+]);
